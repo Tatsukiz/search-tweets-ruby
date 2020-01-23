643bbc73acf358334c76692a4317d5160de18942
@snowman <jimmoffitt@yahoo.com>
2020-01-23 16:02:35 -0700

    Create tweets-search.rb
    building out template

---
+++ search/tweets-search.rb
@@ -0,0 +1,27 @@
+class TweetsSearch
+
+  require 'json'
+	require 'yaml' #Used for configuration files.
+	require 'base64' #Needed if managing encrypted passwords.
+	require 'fileutils'
+	require 'time'
+
+	#Common classes
+	require_relative '../common/requester'
+	require_relative '../common/queries'
+	require_relative '../common/url_maker'
+	require_relative '../common/utilities.rb' #Mixin code.
+
+  #Make initial request, and look for 'next' token, and re-request until the 'next' token is no longer provided.
+	def get_data(rule, start_time, end_time, since_id, until_id)
+
+		loop do
+	
+			next_token, result_count = make_data_request(rule, start_time, end_time, since_id, until_id, @max_results, @format, next_token)
+			break if next_token.nil? or @requester.request_count >= @exit_after
+
+		end
+	
+	end #get_data
+
+end #TweetSearch class.
