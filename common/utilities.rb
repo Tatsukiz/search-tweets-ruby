311028c32677826d01d0ef5e052cc5e2e17eebac
@snowman <jimmoffitt@yahoo.com>
2020-01-23 15:47:45 -0700

    Update utilities.rb

--- common/utilities.rb
+++ common/utilities.rb
@@ -1,20 +1,30 @@
 #Collection of general helper methods.
+#
+# So far, things like
+# + date object helpers.
+# + Support for a variety of timestamp formats.
+# + One file-handling helper.
 
 module Utilities
 
-	#-----------------------------------------------------
-
-	#Confirm a directory exists, creating it if necessary.
-	def Utilities.checkDirectory(directory)
+	def Utilities.checkDirectory(directory) #Confirm a directory exists, creating it if necessary.
 		#Make sure directory exists, making it if needed.
 		if not File.directory?(directory)
 			FileUtils.mkpath(directory) #logging and user notification.
 		end
 		directory
-	end
+  end
+
+  def Utilities.numeric?(object)
+    true if Float(object) rescue false
+  end
 
 	def Utilities.get_date_string(time)
 		return time.year.to_s + sprintf('%02i', time.month) + sprintf('%02i', time.day) + sprintf('%02i', time.hour) + sprintf('%02i', time.min)
+	end
+
+	def Utilities.get_date_ISO_string(time)
+		return "#{time.year.to_s}-#{sprintf('%02i', time.month)}-#{sprintf('%02i', time.day)}T#{sprintf('%02i', time.hour)}:#{sprintf('%02i', time.min)}:00Z"
 	end
 
 	def Utilities.get_date_object(time_string)
@@ -23,11 +33,7 @@ module Utilities
 		return time
 	end
 
-	def Utilities.numeric?(object)
-		true if Float(object) rescue false
-	end
-
-	#Takes a variety of string inputs and returns a standard Twitter Search/PowerTrack YYYYMMDDHHMM timestamp string.
+	#Takes a variety of string inputs and returns a standard Twitter Labs timestamp string.
 	def Utilities.set_date_string(input)
 
 		now = Time.new
@@ -36,41 +42,41 @@ module Utilities
 		#Handle minute notation.
 		if input.downcase[-1] == "m"
 			date = now - (60 * input[0..-2].to_f)
-			return get_date_string(date)
+		  return get_date_ISO_string(date)
 		end
 
 		#Handle hour notation.
 		if input.downcase[-1] == "h"
 			date = now - (60 * 60 * input[0..-2].to_f)
-			return get_date_string(date)
+      return get_date_ISO_string(date)
 		end
 
 		#Handle day notation.
 		if input.downcase[-1] == "d"
 			date = now - (24 * 60 * 60 * input[0..-2].to_f)
-			return get_date_string(date)
+			return get_date_ISO_string(date)
 		end
 
-		#Handle PowerTrack format, YYYYMMDDHHMM
+		#Handle premium/enterprise format, YYYYMMDDHHMM
 		if input.length == 12 and numeric?(input)
-			return input
+			date = Time.new(input[0..3],input[4..5],input[6..7],input[8..9],input[10..11])
+			return get_date_ISO_string(date)
 		end
 
 		#Handle "YYYY-MM-DD 00:00"
 		if input.length == 16
-			return input.gsub!(/\W+/, '')
+			date = Time.new(input[0..3],input[5..6],input[8..9],input[11..12],input[14..15])
+      return get_date_ISO_string(date)
 		end
 
 		#Handle ISO 8601 timestamps, as in Twitter payload "2013-11-15T17:16:42.000Z"
 		if input.length > 16
 			date = Time.parse(input)
-			return get_date_string(date)
+			return get_date_ISO_string(date)
 		end
 
 		return 'Error, unrecognized timestamp.'
 
 	end
-
-	#-----------------------------------------------------
 
 end
